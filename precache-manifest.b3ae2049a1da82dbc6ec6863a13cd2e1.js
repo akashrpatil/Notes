792c265e7d643c2a8f70b1386dc75fbe63941d13
penetrationexpert <penetration.testing.expert@gmail.com>
2020-08-31 14:05:47 +0530

    Updates

---
+++ precache-manifest.b3ae2049a1da82dbc6ec6863a13cd2e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc439973fc294cc069428ab845689e4",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "13517ca1eefd4cadbf8d",
+    "url": "/Notes/static/css/main.34f20c99.chunk.css"
+  },
+  {
+    "revision": "c6cf6b1b94b3a19f74bf",
+    "url": "/Notes/static/js/2.3ccca73a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Notes/static/js/2.3ccca73a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13517ca1eefd4cadbf8d",
+    "url": "/Notes/static/js/main.38f47afa.chunk.js"
+  },
+  {
+    "revision": "66efa363fa8d56764ed4",
+    "url": "/Notes/static/js/runtime-main.d9f0e0c5.js"
+  }
+]);
